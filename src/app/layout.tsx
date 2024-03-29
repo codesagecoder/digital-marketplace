5557436e9a9872a18be83f89a313599e6c9180a6
codesage <laughingsagecoder@gmail.com>
2024-03-29 20:38:20 +0200

    signup server response handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,9 @@ import './globals.css'
 import { cn } from '@/lib/utils'
 import Navbar from '@/components/Navbar'
 import Providers from '@/components/Providers'
+import { Toaster } from 'sonner'
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,6 +27,7 @@ export default function RootLayout({
             <div className='flex-grow flex-1'>{children}</div>
           </Providers>
         </main>
+        <Toaster position='top-center' richColors />
       </body>
     </html>
   )
