421b8a469ed8395af8ca88a0a0f3da81b2268f80
codesage <laughingsagecoder@gmail.com>
2024-03-30 19:36:24 +0200

    building the signin procedure

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -3,6 +3,7 @@ import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import payload from "payload";
 
 export const authRouter = router({
   createPayloadUser: publicProcedure.input(AuthCredentialsValidator).mutation(async ({ input }) => {
@@ -37,4 +38,17 @@ export const authRouter = router({
 
     return { success: true };
   }),
+  signIn: publicProcedure.input(AuthCredentialsValidator).mutation(async ({ input, ctx }) => {
+    try {
+      await payload.login({
+        collection: 'users',
+        data: input,
+        res: ctx.res,
+      });
+
+      return { success: true };
+    } catch (error) {
+      throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+  })
 })
