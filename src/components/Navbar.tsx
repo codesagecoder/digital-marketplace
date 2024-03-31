1e61238cdbd38408e58cafdb841445e2dc3bd6df
codesage <laughingsagecoder@gmail.com>
2024-03-31 20:04:04 +0200

    building the navbar account menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,11 @@ import { buttonVariants } from "./ui/button";
 import Cart from "./Cart";
 import { getServerSideUser } from "@/lib/payload-utils";
 import { cookies } from 'next/headers';
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
   const nextCookies = cookies();
   const { user } = await getServerSideUser(nextCookies);
-  // const user = null;
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -31,7 +31,7 @@ const Navbar = async () => {
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                   {user ? (
                     <>
-                      <p></p>
+                      <UserAccountNav user={user} />
                       <span className="h-6 w-px bg-gray-200" aria-hidden='true' />
                     </>
                   ) : (
