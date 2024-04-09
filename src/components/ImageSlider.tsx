ff44185ea50bda3ba6be31346265d7130e6d6949
codesage <laughingsagecoder@gmail.com>
2024-04-09 21:22:37 +0200

    creating the product image slider

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,95 @@
+import { cn } from "@/lib/utils";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import type SwiperType from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+interface ImageSliderProps {
+  urls: string[];
+}
+
+const ImageSlider = ({ urls }: ImageSliderProps) => {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+
+  });
+
+  useEffect(() => {
+    swiper?.on('slideChange', ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1
+      });
+    })
+  }, [swiper, urls]);
+
+  const activeStyles = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 place-items-center rounded-full border-2 bg-white border-zinc-300";
+  const inactiveStyles = 'hidden to-gray-400';
+
+  return (
+    <div className="group relative bg-zinc-100 overflow-hidden rounded-xl">
+      <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev();
+          }}
+          className={cn(
+            activeStyles,
+            'left-3 transition',
+            {
+              [inactiveStyles]: slideConfig.isBeginning,
+              'hover:bg-purple-300 text-primary-800 opacity-100': !slideConfig.isBeginning,
+            }
+          )}
+          aria-label="previous image">
+          <ChevronLeft className="h-4 w-4 to-zinc-700" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
+          className={cn(
+            activeStyles,
+            'right-3 transition',
+            {
+              [inactiveStyles]: slideConfig.isEnd,
+              'hover:bg-purple-300 text-primary-800 opacity-100': !slideConfig.isEnd,
+            }
+          )}
+          aria-label="next image">
+          <ChevronRight className="h-4 w-4 to-zinc-700" />
+        </button>
+      </div>
+
+      <Swiper
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"></span>`;
+          }
+        }}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        spaceBetween={50}
+        modules={[Pagination]}
+        slidesPerView={1}
+        className="h-full w-full">
+        {urls.map((url, i) => (
+          <SwiperSlide key={i} className="-z-10 relative h-full w-full">
+            <Image alt="product image" src={url} fill loading="eager" className="-z-10 h-full w-full object-cover object-center" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
+};
+
+export default ImageSlider;
--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -6,6 +6,7 @@ import { Skeleton } from "./ui/skeleton";
 import Link from "next/link";
 import { cn, formatPrice } from "@/lib/utils";
 import { PRODUCT_CATEGORIES } from "@/config";
+import ImageSlider from "./ImageSlider";
 
 interface ProductListingProps {
   product: Product | null;
@@ -25,6 +26,7 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
 
   if (isVisible && product) {
     const label = PRODUCT_CATEGORIES.find(({ value }) => value === product.category)?.label;
+    const validUrls = product.images.map(({ image }) => (typeof image === 'string' ? image : image?.url)).filter(Boolean) as string[];
 
     return (
       <Link
@@ -34,6 +36,7 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
         )}
         href={`/product/${product.id}`}>
         <div className="flex flex-col w-full">
+          <ImageSlider urls={validUrls} />
           <h3 className="mt-4 font-medium text-sm text-gray-700">{product.name}</h3>
           <p className="mt-1 text-sm to-gray-500">{label}</p>
           <p className="mt-1 font-medium text-sm to-gray-900">{formatPrice(product.price)}</p>
