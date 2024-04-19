6d3bc86747c91e3215cbe1060382b203c0f1c465
codesage <laughingsagecoder@gmail.com>
2024-04-19 18:36:13 +0200

    building add to cart button ui

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+const AddToCartButton = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [isSuccess])
+
+  return (
+    <Button onClick={() => setIsSuccess(true)} size='lg' className="w-full">
+      {isSuccess ? 'Added!' : 'Add to cart'}
+    </Button>
+  )
+
+}
+
+export default AddToCartButton;
